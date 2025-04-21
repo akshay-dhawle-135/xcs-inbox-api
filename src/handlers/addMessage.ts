699db0a4929b57edb0da0cbf0892cbf7396b0cf8
akshay-dhawle-135 <akshay.dhawle@xplortechnologies.com>
2025-04-21 14:37:13 +0530

    initial commit

---
+++ src/handlers/addMessage.ts
@@ -0,0 +1,17 @@
+import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const handler = async (
+  event: APIGatewayEvent,
+): Promise<APIGatewayProxyResult> => {
+  console.log("Received event:", event);  
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: "Message Added!",
+      input: event,
+    }),
+  };
+
+  return response;
+};
