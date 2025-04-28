87459f3ba9d08dfc8090134282d3c1d13f32a9c3
akshay-dhawle-135 <akshay.dhawle@xplortechnologies.com>
2025-04-28 12:36:23 +0530

    updated policy

--- src/handlers/addConversation.ts
+++ src/handlers/addConversation.ts
@@ -4,6 +4,7 @@ export const handler = async (
   event: APIGatewayEvent,
 ): Promise<APIGatewayProxyResult> => {
   console.log("Received event:", event);  
+  
 
   const response = {
     statusCode: 200,
